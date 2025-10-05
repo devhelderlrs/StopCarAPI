ea0da8984e7bb237a002b4555382aef12497cc00
devhelderlrs <helderl4004@gmail.com>
2025-10-05 07:42:22 -0300

    envio arquivos debug

--- obj/Debug/net8.0/StopCar.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/StopCar.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StopCar.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a1dd5571e22840d92a2756ba26ac672f209c10f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a85b4450802a808b752fcce85a629ddf410ea74f")]
 [assembly: System.Reflection.AssemblyProductAttribute("StopCar.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StopCar.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
